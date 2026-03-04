a6a86caa6a002ba529286afec63a92ab676f097b
Sonia Chicaiza <snchicaiza@gmail.com>
2026-03-03 23:05:17 -0500

    departamentos modificación

--- S8EvaluaciónPráctica2P/Models/Department.cs
+++ S8EvaluaciónPráctica2P/Models/Department.cs
@@ -4,86 +4,22 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace S8EvaluaciónPráctica2P.Models
 {
-    public class Department : Controller
+    [Table("departments")]
+    public class Department
     {
-        // GET: Department
-        public ActionResult Index()
-        {
-            return View();
-        }
+        [Key]
+        [StringLength(4)]
+        [Display(Name = "Código Depto.")]
+        public string dept_no { get; set; }
 
-        // GET: Department/Details/5
-        public ActionResult Details(int id)
-        {
-            return View();
-        }
-
-        // GET: Department/Create
-        public ActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: Department/Create
-        [HttpPost]
-        public ActionResult Create(FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add insert logic here
-
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: Department/Edit/5
-        public ActionResult Edit(int id)
-        {
-            return View();
-        }
-
-        // POST: Department/Edit/5
-        [HttpPost]
-        public ActionResult Edit(int id, FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add update logic here
-
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: Department/Delete/5
-        public ActionResult Delete(int id)
-        {
-            return View();
-        }
-
-        // POST: Department/Delete/5
-        [HttpPost]
-        public ActionResult Delete(int id, FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add delete logic here
-
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
-        }
+        [Required]
+        [StringLength(40)]
+        [Display(Name = "Nombre Departamento")]
+        public string dept_name { get; set; }
     }
 }
